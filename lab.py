d7113abf503250dbb13517fed77a07f286ea1996
Krysthian Lessa <krysthianlessa@gmai.com>
2020-07-15 22:20:28 -0300

    Start main.py over again

---
+++ lab.py
@@ -0,0 +1,10 @@
+class Lab:
+
+    def __init__(self):
+        self.users = []
+
+    def to_monitor():
+        pass
+
+    def register_user():
+        pass
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-
+from lab import Lab
+from student import Student
+from teacher import Teacher
 
 def main():
-    pass
+    led = Lab()
 
-
-
+    
 
 if __name__ == "__main__":
     main()
